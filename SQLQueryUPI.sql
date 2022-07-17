e7934a06c3604b3414d66560b4d6aea483a3e490
Roy Duran <rduran80@hotmail.com>
2022-07-17 02:25:06 -0600

    creacion procedimientos almacenados en BD

--- SQLQueryUPI.sql
+++ SQLQueryUPI.sql
@@ -13,7 +13,7 @@ create table usuario(
 select * from usuario
 
 --consulta con parametro
-select nombre from usuario where clave='123'
+select nombre from usuario where clave='789'
 
 --borar tabla
 drop table usuario
@@ -54,3 +54,80 @@ delete articulos where codigo = 3
 
 Select SYSDATETIME() as [SYSDATETIME]
 
+--***** Procesos almacenados usuario *********
+--Crer preoceso
+create proc ConsultaUsuario
+	as
+	begin
+	select * from usuario
+	end
+
+create proc ConsultaUsuarioFiltro
+	@nombre varchar(50) 
+	as
+	begin
+	select * from usuario where nombre = @nombre
+	end
+
+create proc BorrarUsuario
+	@nombre varchar(50) 
+	as
+	begin
+	delete usuario where nombre = @nombre
+	end
+
+create proc ActualizarUsuario
+	@codigo int,
+	@nombre varchar(50),
+	@clave varchar(30)
+	as
+	begin
+	update usuario set nombre = @nombre, clave = @clave where codigo = @codigo
+	end
+
+create proc ConsultaArticulo
+	as
+	begin
+	select * from articulos
+	end
+
+create proc ConsultaArticuloFiltro
+	@codigo int 
+	as
+	begin
+	select * from articulos where codigo = @codigo
+	end
+
+create proc BorrarArticulo
+	@codigo int 
+	as
+	begin
+	delete articulos where codigo = @codigo
+	end
+
+create proc ActualizarArticulo
+	@codigo int,
+	@descripcion varchar(100),
+	@cantidad int,
+	@precio float,
+	@fechaIngreso date
+	as
+	begin
+	update articulos set descripcion = @descripcion, cantidad = @cantidad, precio=@precio, fechaIngreso=@fechaIngreso where codigo = @codigo
+	end
+
+--Ejecutar el proceso
+-- Para usuarios
+execute ConsultaUsuario
+execute ConsultaUsuarioFiltro 'Ana'
+execute BorrarUsuario 'Ana'
+execute BorrarUsuario 'Ana'
+execute ActualizarUsuario 15,'Andrea','123'
+
+-- Para articulos
+execute ConsultaArticulo
+execute ConsultaArticuloFiltro 11
+execute BorrarArticulo 11
+execute ActualizarArticulo 7,'Paquete de hojas blancas 100 U',200,1500,'2022-07-13'
+
+drop proc ActualizarUsuario
